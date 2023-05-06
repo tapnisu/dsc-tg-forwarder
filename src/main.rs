d5ec9b88f7c82c90dbeb4adf83e9584f1a131b7e
Alexiy Rybin <0xtapnisu@gmail.com>
2023-05-06 14:51:17 +0700

    Create simple messages logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, _ctx: Context, new_message: Message) {
+        println!("{}", new_message.content)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
