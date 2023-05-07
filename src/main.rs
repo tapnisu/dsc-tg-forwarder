e80a22f43b2423a9e9229ce8a7264bd7e301d897
Alexiy Rybin <0xtapnisu@gmail.com>
2023-05-08 00:17:25 +0700

    Make embed a bit better

--- src/main.rs
+++ src/main.rs
@@ -29,41 +29,49 @@ struct Arguments {
 }
 
 fn format_embed(embed: Embed) -> String {
-    let mut res = "".to_owned();
+    let mut res = "Embed:".to_owned();
 
     if let Some(title) = embed.title {
-        res += &format!("Title: {}\n", title);
+        res += &format!(
+            "[{}]({})\n",
+            escape_markdownv2(title),
+            embed.url.unwrap_or("".to_string())
+        );
     }
+
     if let Some(description) = embed.description {
-        res += &format!("Description: {}\n", description);
-    }
-    if let Some(url) = embed.url {
-        res += &format!("Url: {}\n", url);
+        res += &format!("{}\n", escape_markdownv2(description));
     }
 
     res += &embed
         .fields
         .iter()
-        .map(|f| format!("\n  {}\n  {}\n", f.name, f.value))
+        .map(|f| {
+            format!(
+                "\n{}\n{}\n",
+                escape_markdownv2(f.name.clone()),
+                escape_markdownv2(f.value.clone())
+            )
+        })
         .collect::<String>();
 
     if let Some(thumbnail) = embed.thumbnail {
-        res += &format!("Thumbnail: {}\n", thumbnail.url);
+        res += &format!("Thumbnail: {}\n", escape_markdownv2(thumbnail.url));
     }
     if let Some(image) = embed.image {
-        res += &format!("Image: {}\n", image.url);
+        res += &format!("Image: {}\n", escape_markdownv2(image.url));
     }
     if let Some(video) = embed.video {
-        res += &format!("Video: {}\n", video.url);
+        res += &format!("Video: {}\n", escape_markdownv2(video.url));
     }
     if let Some(author) = embed.author {
-        res += &format!("Author: {}\n", author.name);
+        res += &format!("Author: {}\n", escape_markdownv2(author.name));
     }
     if let Some(footer) = embed.footer {
-        res += &format!("Footer: {}\n", footer.text);
+        res += &format!("Footer: {}\n", escape_markdownv2(footer.text));
     }
     if let Some(timestamp) = embed.timestamp {
-        res += &format!("Timestamp: {}\n", timestamp);
+        res += &format!("Timestamp: {}\n", escape_markdownv2(timestamp));
     }
 
     res
@@ -106,30 +114,34 @@ impl EventHandler for Handler {
         self.bot
             .send_message(
                 self.output_channel_id.clone(),
-                escape_markdownv2(format!(
+                format!(
                     "[{}]: {}\n{}",
                     if msg.is_private() {
-                        msg.author.tag()
+                        escape_markdownv2(msg.author.tag())
                     } else {
                         format!(
                             "{} / {} / {}",
-                            Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name,
-                            msg.channel_id
-                                .to_channel(&ctx.http)
-                                .await
-                                .unwrap()
-                                .guild()
-                                .unwrap()
-                                .name,
-                            msg.author.tag(),
+                            escape_markdownv2(
+                                Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name
+                            ),
+                            escape_markdownv2(
+                                msg.channel_id
+                                    .to_channel(&ctx.http)
+                                    .await
+                                    .unwrap()
+                                    .guild()
+                                    .unwrap()
+                                    .name
+                            ),
+                            escape_markdownv2(msg.author.tag()),
                         )
                     },
-                    msg.content_safe(ctx.cache),
+                    escape_markdownv2(msg.content_safe(ctx.cache)),
                     msg.embeds
                         .into_iter()
                         .map(|e| format_embed(e))
                         .collect::<String>()
-                )),
+                ),
             )
             .parse_mode(ParseMode::MarkdownV2)
             .await
