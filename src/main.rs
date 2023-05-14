c65916245cd8052fa0ea01c6cb23e9574f4990fd
Alexiy Rybin <0xtapnisu@gmail.com>
2023-05-14 09:39:02 +0700

    Move message formatting to function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod config;
+mod utils;
 
 use crate::config::parse_config;
 use clap::Parser;
 use home;
 use serenity::async_trait;
 use serenity::model::channel::Message;
-use serenity::model::prelude::{Embed, Guild};
 use serenity::prelude::*;
 use std::env;
 use teloxide::prelude::*;
 use teloxide::types::ParseMode;
+use utils::format_message;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -28,84 +29,9 @@ struct Arguments {
     config_path: Option<String>,
 }
 
-fn format_embed(embed: Embed) -> String {
-    let mut res = "Embed:".to_owned();
-
-    if let Some(title) = embed.title {
-        res += &format!(
-            "[{}]({})\n",
-            escape_markdownv2(title),
-            embed.url.unwrap_or("".to_string())
-        );
-    }
-
-    if let Some(description) = embed.description {
-        res += &format!("{}\n", escape_markdownv2(description));
-    }
-
-    res += &embed
-        .fields
-        .iter()
-        .map(|f| {
-            format!(
-                "\n{}\n{}\n",
-                escape_markdownv2(f.name.clone()),
-                escape_markdownv2(f.value.clone())
-            )
-        })
-        .collect::<String>();
-
-    if let Some(thumbnail) = embed.thumbnail {
-        res += &format!("Thumbnail: {}\n", escape_markdownv2(thumbnail.url));
-    }
-    if let Some(image) = embed.image {
-        res += &format!("Image: {}\n", escape_markdownv2(image.url));
-    }
-    if let Some(video) = embed.video {
-        res += &format!("Video: {}\n", escape_markdownv2(video.url));
-    }
-    if let Some(author) = embed.author {
-        res += &format!("Author: {}\n", escape_markdownv2(author.name));
-    }
-    if let Some(footer) = embed.footer {
-        res += &format!("Footer: {}\n", escape_markdownv2(footer.text));
-    }
-    if let Some(timestamp) = embed.timestamp {
-        res += &format!("Timestamp: {}\n", escape_markdownv2(timestamp));
-    }
-
-    res
-}
-
 struct Handler {
     bot: Bot,
     output_channel_id: String,
-}
-
-fn escape_markdownv2(text: String) -> String {
-    text.chars()
-        .map(|x| match x {
-            '_' => "\\_".to_string(),
-            '*' => "\\*".to_string(),
-            '[' => "\\[".to_string(),
-            ']' => "\\]".to_string(),
-            '(' => "\\(".to_string(),
-            ')' => "\\)".to_string(),
-            '~' => "\\~".to_string(),
-            '`' => "\\`".to_string(),
-            '>' => "\\>".to_string(),
-            '#' => "\\#".to_string(),
-            '+' => "\\+".to_string(),
-            '-' => "\\-".to_string(),
-            '=' => "\\=".to_string(),
-            '|' => "\\|".to_string(),
-            '{' => "\\{".to_string(),
-            '}' => "\\}".to_string(),
-            '.' => "\\.".to_string(),
-            '!' => "\\!".to_string(),
-            _ => x.to_owned().to_string(),
-        })
-        .collect()
 }
 
 #[async_trait]
@@ -114,34 +40,7 @@ impl EventHandler for Handler {
         self.bot
             .send_message(
                 self.output_channel_id.clone(),
-                format!(
-                    "[{}]: {}\n{}",
-                    if msg.is_private() {
-                        escape_markdownv2(msg.author.tag())
-                    } else {
-                        format!(
-                            "{} / {} / {}",
-                            escape_markdownv2(
-                                Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name
-                            ),
-                            escape_markdownv2(
-                                msg.channel_id
-                                    .to_channel(&ctx.http)
-                                    .await
-                                    .unwrap()
-                                    .guild()
-                                    .unwrap()
-                                    .name
-                            ),
-                            escape_markdownv2(msg.author.tag()),
-                        )
-                    },
-                    escape_markdownv2(msg.content_safe(ctx.cache)),
-                    msg.embeds
-                        .into_iter()
-                        .map(|e| format_embed(e))
-                        .collect::<String>()
-                ),
+                format_message(ctx, msg).await,
             )
             .parse_mode(ParseMode::MarkdownV2)
             .await
---
+++ src/utils/mod.rs
@@ -0,0 +1,108 @@
+use serenity::{
+    model::prelude::{Embed, Guild, Message},
+    prelude::Context,
+};
+
+pub fn escape_markdownv2(text: String) -> String {
+    text.chars()
+        .map(|x| match x {
+            '_' => "\\_".to_string(),
+            '*' => "\\*".to_string(),
+            '[' => "\\[".to_string(),
+            ']' => "\\]".to_string(),
+            '(' => "\\(".to_string(),
+            ')' => "\\)".to_string(),
+            '~' => "\\~".to_string(),
+            '`' => "\\`".to_string(),
+            '>' => "\\>".to_string(),
+            '#' => "\\#".to_string(),
+            '+' => "\\+".to_string(),
+            '-' => "\\-".to_string(),
+            '=' => "\\=".to_string(),
+            '|' => "\\|".to_string(),
+            '{' => "\\{".to_string(),
+            '}' => "\\}".to_string(),
+            '.' => "\\.".to_string(),
+            '!' => "\\!".to_string(),
+            _ => x.to_owned().to_string(),
+        })
+        .collect()
+}
+
+pub async fn format_message(ctx: Context, msg: Message) -> String {
+    format!(
+        "[{}]: {}\n{}",
+        if msg.is_private() {
+            escape_markdownv2(msg.author.tag())
+        } else {
+            format!(
+                "{} / {} / {}",
+                escape_markdownv2(Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name),
+                escape_markdownv2(
+                    msg.channel_id
+                        .to_channel(&ctx.http)
+                        .await
+                        .unwrap()
+                        .guild()
+                        .unwrap()
+                        .name
+                ),
+                escape_markdownv2(msg.author.tag()),
+            )
+        },
+        escape_markdownv2(msg.content_safe(ctx.cache)),
+        msg.embeds
+            .into_iter()
+            .map(|e| format_embed(e))
+            .collect::<String>()
+    )
+}
+
+pub fn format_embed(embed: Embed) -> String {
+    let mut res = "Embed:\n".to_owned();
+
+    if let Some(title) = embed.title {
+        res += &format!(
+            "[{}]({})\n",
+            escape_markdownv2(title),
+            embed.url.unwrap_or("".to_string())
+        );
+    }
+
+    if let Some(description) = embed.description {
+        res += &format!("{}\n", escape_markdownv2(description));
+    }
+
+    res += &embed
+        .fields
+        .iter()
+        .map(|f| {
+            format!(
+                "\n{}\n{}\n",
+                escape_markdownv2(f.name.clone()),
+                escape_markdownv2(f.value.clone())
+            )
+        })
+        .collect::<String>();
+
+    if let Some(thumbnail) = embed.thumbnail {
+        res += &format!("Thumbnail: {}\n", escape_markdownv2(thumbnail.url));
+    }
+    if let Some(image) = embed.image {
+        res += &format!("Image: {}\n", escape_markdownv2(image.url));
+    }
+    if let Some(video) = embed.video {
+        res += &format!("Video: {}\n", escape_markdownv2(video.url));
+    }
+    if let Some(author) = embed.author {
+        res += &format!("Author: {}\n", escape_markdownv2(author.name));
+    }
+    if let Some(footer) = embed.footer {
+        res += &format!("Footer: {}\n", escape_markdownv2(footer.text));
+    }
+    if let Some(timestamp) = embed.timestamp {
+        res += &format!("Timestamp: {}\n", escape_markdownv2(timestamp));
+    }
+
+    res
+}
