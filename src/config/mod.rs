a1d7a37cf26e8ef1091b95eb54c02ff5827a2d58
Alexiy Rybin <0xtapnisu@gmail.com>
2023-06-12 18:07:40 +0700

    Clean up code

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,20 +3,30 @@ use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
 
+fn empty_ids_vec() -> Vec<u64> {
+    vec![]
+}
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Config {
     pub discord_token: Option<String>,
     pub telegram_token: Option<String>,
     pub output_chat_id: Option<String>,
 
-    pub allowed_guilds_ids: Option<Vec<u64>>,
-    pub muted_guilds_ids: Option<Vec<u64>>,
+    #[serde(default = "empty_ids_vec")]
+    pub allowed_guilds_ids: Vec<u64>,
+    #[serde(default = "empty_ids_vec")]
+    pub muted_guilds_ids: Vec<u64>,
 
-    pub allowed_channels_ids: Option<Vec<u64>>,
-    pub muted_channels_ids: Option<Vec<u64>>,
+    #[serde(default = "empty_ids_vec")]
+    pub allowed_channels_ids: Vec<u64>,
+    #[serde(default = "empty_ids_vec")]
+    pub muted_channels_ids: Vec<u64>,
 
-    pub allowed_users_ids: Option<Vec<u64>>,
-    pub muted_users_ids: Option<Vec<u64>>,
+    #[serde(default = "empty_ids_vec")]
+    pub allowed_users_ids: Vec<u64>,
+    #[serde(default = "empty_ids_vec")]
+    pub muted_users_ids: Vec<u64>,
 }
 
 pub fn parse_config(path: String) -> Config {
