6fa82df602256773645b9d9e5eb3258d24057279
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-16 12:28:40 +0700

    Cli struct

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Discord token (if not used will be taken from DISCORD_TOKEN)
+    #[clap(short, long)]
+    pub discord_token: Option<String>,
+    /// Telegram token (if not used will be taken from TELEGRAM_TOKEN)
+    #[clap(short, long)]
+    pub telegram_token: Option<String>,
+    /// Id of telegram user/group to send output to
+    #[clap(short, long)]
+    pub output_chat_id: Option<String>,
+    /// Path to configuration file (default is ~/.config/dsc-tg-forwarder/config.yml)
+    #[clap(short, long)]
+    pub config_path: Option<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod cli;
 mod config;
 mod utils;
 
 use crate::config::parse_config;
 use clap::Parser;
+use cli::Cli;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
@@ -10,23 +12,6 @@ use std::env;
 use teloxide::prelude::*;
 use teloxide::types::ParseMode;
 use utils::format_message;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Arguments {
-    /// Discord token (if not used will be taken from DISCORD_TOKEN)
-    #[clap(short, long)]
-    discord_token: Option<String>,
-    /// Telegram token (if not used will be taken from TELEGRAM_TOKEN)
-    #[clap(short, long)]
-    telegram_token: Option<String>,
-    /// Id of telegram user/group to send output to
-    #[clap(short, long)]
-    output_chat_id: Option<String>,
-    /// Path to configuration file (default is ~/.config/dsc-tg-forwarder/config.yml)
-    #[clap(short, long)]
-    config_path: Option<String>,
-}
 
 struct Handler {
     bot: Bot,
@@ -74,7 +59,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let args = Arguments::parse();
+    let args = Cli::parse();
     let config = parse_config(args.config_path.unwrap_or(format!(
         "{}/.config/dsc-tg-forwarder/config.yml",
         home::home_dir().unwrap().display()
