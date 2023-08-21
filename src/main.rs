84a1500f4e967dd5af68212f540a1abc09000d71
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-21 14:09:00 +0700

    move out tokens and chat id

--- src/main.rs
+++ src/main.rs
@@ -60,30 +60,35 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
-    let config = parse_config(args.config_path.unwrap_or(format!(
+    let config_path = args.config_path.unwrap_or(format!(
         "{}/.config/dsc-tg-forwarder/config.yml",
         home::home_dir().unwrap().display()
-    )));
+    ));
+    let config = parse_config(config_path);
+
+    let discord_token = args.discord_token.unwrap_or(
+        config
+            .discord_token
+            .unwrap_or_else(|| env::var("DISCORD_TOKEN").expect("Discord token wasn't supplied")),
+    );
+
+    let telegram_token =
+        args.telegram_token
+            .unwrap_or(config.telegram_token.unwrap_or_else(|| {
+                env::var("TELEGRAM_TOKEN").expect("Telegram token wasn't supplied")
+            }));
+
+    let output_chat_id =
+        args.output_chat_id
+            .unwrap_or(config.output_chat_id.unwrap_or_else(|| {
+                env::var("OUTPUT_CHAT_ID").expect("Output chat id wasn't supplied")
+            }));
 
     // Login with a bot token from the environment
-    let mut client =
-        Client::builder(args.discord_token.unwrap_or(
-            config.discord_token.unwrap_or_else(|| {
-                env::var("DISCORD_TOKEN").expect("Discord token wasn't supplied")
-            }),
-        ))
+    let mut client = Client::builder(discord_token)
         .event_handler(Handler {
-            bot: Bot::new(
-                args.telegram_token
-                    .unwrap_or(config.telegram_token.unwrap_or_else(|| {
-                        env::var("TELEGRAM_TOKEN").expect("Telegram token wasn't supplied")
-                    })),
-            ),
-            output_chat_id: args
-                .output_chat_id
-                .unwrap_or(config.output_chat_id.unwrap_or_else(|| {
-                    env::var("OUTPUT_CHAT_ID").expect("Output chat id wasn't supplied")
-                })),
+            bot: Bot::new(telegram_token),
+            output_chat_id,
             allowed_guilds_ids: config.allowed_guilds_ids,
             muted_guilds_ids: config.muted_guilds_ids,
             allowed_channels_ids: config.allowed_channels_ids,
