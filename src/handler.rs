e11774c4d4adf104d79e777be6bccc9993a70a23
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-21 14:21:24 +0700

    move handler to another file

---
+++ src/handler.rs
@@ -0,0 +1,50 @@
+use crate::utils::format_message;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+use teloxide::prelude::*;
+use teloxide::types::ParseMode;
+
+pub struct Handler {
+    pub bot: Bot,
+    pub output_chat_id: String,
+
+    pub allowed_guilds_ids: Vec<u64>,
+    pub muted_guilds_ids: Vec<u64>,
+
+    pub allowed_channels_ids: Vec<u64>,
+    pub muted_channels_ids: Vec<u64>,
+
+    pub allowed_users_ids: Vec<u64>,
+    pub muted_users_ids: Vec<u64>,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if (!self
+            .allowed_guilds_ids
+            .contains(&msg.guild_id.unwrap_or_default().0)
+            && !self.allowed_guilds_ids.is_empty())
+            || (self
+                .muted_guilds_ids
+                .contains(&msg.guild_id.unwrap_or_default().0)
+                && !self.muted_guilds_ids.is_empty())
+            || (!self.allowed_channels_ids.contains(&msg.channel_id.0)
+                && !self.allowed_channels_ids.is_empty())
+            || (self.muted_channels_ids.contains(&msg.channel_id.0)
+                && !self.muted_channels_ids.is_empty())
+            || (!self.allowed_users_ids.contains(&msg.author.id.0)
+                && !self.allowed_users_ids.is_empty())
+            || (self.muted_users_ids.contains(&msg.author.id.0) && !self.muted_users_ids.is_empty())
+        {
+            return;
+        }
+
+        self.bot
+            .send_message(self.output_chat_id.clone(), format_message(ctx, msg).await)
+            .parse_mode(ParseMode::MarkdownV2)
+            .await
+            .unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,15 @@
 mod cli;
 mod config;
+mod handler;
 mod utils;
 
-use crate::config::parse_config;
-use clap::Parser;
+use clap::{error::ErrorKind, CommandFactory, Parser};
 use cli::Cli;
-use serenity::async_trait;
-use serenity::model::channel::Message;
+use config::parse_config;
+use handler::Handler;
 use serenity::prelude::*;
 use std::env;
 use teloxide::prelude::*;
-use teloxide::types::ParseMode;
-use utils::format_message;
-
-struct Handler {
-    bot: Bot,
-    output_chat_id: String,
-
-    allowed_guilds_ids: Vec<u64>,
-    muted_guilds_ids: Vec<u64>,
-
-    allowed_channels_ids: Vec<u64>,
-    muted_channels_ids: Vec<u64>,
-
-    allowed_users_ids: Vec<u64>,
-    muted_users_ids: Vec<u64>,
-}
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if (!self
-            .allowed_guilds_ids
-            .contains(&msg.guild_id.unwrap_or_default().0)
-            && !self.allowed_guilds_ids.is_empty())
-            || (self
-                .muted_guilds_ids
-                .contains(&msg.guild_id.unwrap_or_default().0)
-                && !self.muted_guilds_ids.is_empty())
-            || (!self.allowed_channels_ids.contains(&msg.channel_id.0)
-                && !self.allowed_channels_ids.is_empty())
-            || (self.muted_channels_ids.contains(&msg.channel_id.0)
-                && !self.muted_channels_ids.is_empty())
-            || (!self.allowed_users_ids.contains(&msg.author.id.0)
-                && !self.allowed_users_ids.is_empty())
-            || (self.muted_users_ids.contains(&msg.author.id.0) && !self.muted_users_ids.is_empty())
-        {
-            return;
-        }
-
-        self.bot
-            .send_message(self.output_chat_id.clone(), format_message(ctx, msg).await)
-            .parse_mode(ParseMode::MarkdownV2)
-            .await
-            .unwrap();
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -85,7 +39,7 @@ async fn main() {
             }));
 
     // Login with a bot token from the environment
-    let mut client = Client::builder(discord_token)
+    let client = Client::builder(discord_token)
         .event_handler(Handler {
             bot: Bot::new(telegram_token),
             output_chat_id,
@@ -96,11 +50,16 @@ async fn main() {
             allowed_users_ids: config.allowed_users_ids,
             muted_users_ids: config.muted_users_ids,
         })
-        .await
-        .expect("Error creating client");
+        .await;
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+    match client {
+        Err(err) => Cli::command().error(ErrorKind::InvalidValue, err).exit(),
+        Ok(mut client) => {
+            // start listening for events by starting a single shard
+
+            if let Err(err) = client.start().await {
+                let _ = Cli::command().error(ErrorKind::InvalidValue, err).print();
+            }
+        }
     }
 }
