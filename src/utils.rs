3e95abe9bc75583a24c5ff08f2a803e7aa5c763d
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-11-24 22:53:19 +0700

    fix []

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use serenity::{
     prelude::Context,
 };
 
-pub fn escape_markdownv2(text: String) -> String {
+pub fn escape_markdownv2(text: &str) -> String {
     text.chars()
         .map(|x| match x {
             '_' => "\\_".to_string(),
@@ -31,15 +31,15 @@ pub fn escape_markdownv2(text: String) -> String {
 
 pub async fn format_message(ctx: Context, msg: Message) -> String {
     format!(
-        "[{}]: {}\n{}",
+        "\\[{}\\]: {}\n{}",
         if msg.is_private() {
-            escape_markdownv2(msg.author.tag())
+            escape_markdownv2(&msg.author.tag())
         } else {
             format!(
                 "{} / {} / {}",
-                escape_markdownv2(Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name),
+                escape_markdownv2(&Guild::get(&ctx, msg.guild_id.unwrap()).await.unwrap().name),
                 escape_markdownv2(
-                    msg.channel_id
+                    &msg.channel_id
                         .to_channel(&ctx.http)
                         .await
                         .unwrap()
@@ -47,10 +47,10 @@ pub async fn format_message(ctx: Context, msg: Message) -> String {
                         .unwrap()
                         .name
                 ),
-                escape_markdownv2(msg.author.tag()),
+                escape_markdownv2(&msg.author.tag()),
             )
         },
-        escape_markdownv2(msg.content_safe(ctx.cache)),
+        escape_markdownv2(&msg.content_safe(ctx.cache)),
         msg.embeds.into_iter().map(format_embed).collect::<String>()
     )
 }
@@ -61,40 +61,40 @@ pub fn format_embed(embed: Embed) -> String {
     if let Some(title) = embed.title {
         res += &format!(
             "[{}]({})\n",
-            escape_markdownv2(title),
+            escape_markdownv2(&title),
             embed.url.unwrap_or("".to_string())
         );
     }
 
     if let Some(description) = embed.description {
-        res += &format!("{}\n", escape_markdownv2(description));
+        res += &format!("{}\n", escape_markdownv2(&description));
     }
 
     res += &embed.fields.iter().fold(String::new(), |acc, f| {
         acc + &format!(
             "\n{}\n{}\n",
-            escape_markdownv2(f.name.clone()),
-            escape_markdownv2(f.value.clone())
+            escape_markdownv2(&f.name.clone()),
+            escape_markdownv2(&f.value.clone())
         )
     });
 
     if let Some(thumbnail) = embed.thumbnail {
-        res += &format!("Thumbnail: {}\n", escape_markdownv2(thumbnail.url));
+        res += &format!("Thumbnail: {}\n", escape_markdownv2(&thumbnail.url));
     }
     if let Some(image) = embed.image {
-        res += &format!("Image: {}\n", escape_markdownv2(image.url));
+        res += &format!("Image: {}\n", escape_markdownv2(&image.url));
     }
     if let Some(video) = embed.video {
-        res += &format!("Video: {}\n", escape_markdownv2(video.url));
+        res += &format!("Video: {}\n", escape_markdownv2(&video.url));
     }
     if let Some(author) = embed.author {
-        res += &format!("Author: {}\n", escape_markdownv2(author.name));
+        res += &format!("Author: {}\n", escape_markdownv2(&author.name));
     }
     if let Some(footer) = embed.footer {
-        res += &format!("Footer: {}\n", escape_markdownv2(footer.text));
+        res += &format!("Footer: {}\n", escape_markdownv2(&footer.text));
     }
     if let Some(timestamp) = embed.timestamp {
-        res += &format!("Timestamp: {}\n", escape_markdownv2(timestamp));
+        res += &format!("Timestamp: {}\n", escape_markdownv2(&timestamp));
     }
 
     res
