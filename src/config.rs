51973a843c7672c95b8f70203bcb0512adcc909f
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-04-05 22:20:07 +0700

    Move config functions to its own file

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,8 @@
+use crate::Cli;
 use serde::{Deserialize, Serialize};
+use std::env;
+use std::error::Error;
+use std::fmt::Display;
 use std::fs::{self, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
@@ -62,3 +66,68 @@ pub fn parse_config(path: &PathBuf) -> anyhow::Result<Config> {
 
     Ok(config)
 }
+
+// Todo: Return struct instead of tuple
+pub fn get_tokens(args: &Cli, config: &Config) -> anyhow::Result<(String, String, String)> {
+    let discord_token = get_discord_token(args, config)?;
+    let telegram_token = get_telegram_token(args, config)?;
+    let output_channel = get_output_channel(args, config)?;
+
+    Ok((discord_token, telegram_token, output_channel))
+}
+
+fn get_telegram_token(args: &Cli, config: &Config) -> anyhow::Result<String> {
+    let telegram_token = match (
+        args.telegram_token.clone(),
+        config.telegram_token.clone(),
+        env::var("TELEGRAM_TOKEN"),
+    ) {
+        (Some(telegram_token), _, _) => telegram_token,
+        (_, Some(telegram_token), _) => telegram_token,
+        (_, _, Ok(telegram_token)) => telegram_token,
+        (None, None, Err(_)) => return Err(ConfigTokenError("Telegram token".to_owned()).into()),
+    };
+
+    Ok(telegram_token)
+}
+
+fn get_discord_token(args: &Cli, config: &Config) -> anyhow::Result<String> {
+    let discord_token = match (
+        args.discord_token.clone(),
+        config.discord_token.clone(),
+        env::var("DISCORD_TOKEN"),
+    ) {
+        (Some(discord_token), _, _) => discord_token,
+        (_, Some(discord_token), _) => discord_token,
+        (_, _, Ok(discord_token)) => discord_token,
+        (None, None, Err(_)) => return Err(ConfigTokenError("Discord token".to_owned()).into()),
+    };
+
+    Ok(discord_token)
+}
+
+fn get_output_channel(args: &Cli, config: &Config) -> anyhow::Result<String> {
+    let output_chat_id = match (
+        args.output_chat_id.clone(),
+        config.output_chat_id.clone(),
+        env::var("OUTPUT_CHAT_ID"),
+    ) {
+        (Some(output_chat_id), _, _) => output_chat_id,
+        (_, Some(output_chat_id), _) => output_chat_id,
+        (_, _, Ok(output_chat_id)) => output_chat_id,
+        (None, None, Err(_)) => return Err(ConfigTokenError("Output channel".to_owned()).into()),
+    };
+
+    Ok(output_chat_id)
+}
+
+#[derive(Clone, Debug)]
+struct ConfigTokenError(String);
+
+impl Display for ConfigTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} not found!", self.0)
+    }
+}
+
+impl Error for ConfigTokenError {}
